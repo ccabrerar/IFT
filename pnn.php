8f071cc59b881c72e3f89d10adc82ec4861e0475
Christian Cabrera <ccabrerar@users.noreply.github.com>
2020-07-17 05:42:02 -0500

    Actualizado 2020-07-17

--- pnn.php
+++ pnn.php
@@ -2,12 +2,12 @@
 /*
 	Genera el archivo .sql a partir del CSV del Plan Nacional de Numeración.
 	La versión más reciente del PNN se consigue desde https://sns.ift.org.mx:8081/sns-frontend/planes-numeracion/descarga-publica.xhtml
-	
+
 	Uso:		php pnn.php  (no recibe argumentos)
 	Requiere:	La existencia del archivo pnn.csv en el mismo directorio
-	
+
 	Este script entrega el archivo .sql en el mismo directorio donde se invocó. No lo carga a la BD.
-	
+
 */
 
 
@@ -26,11 +26,11 @@ if (!file_exists($filename))
 // Revisa el tamaño del archivo pnn.csv. Si es demasiado pequeño, detiene el proceso para no generar un archivo SQL vacío.
 //if (filesize($filename)
 
-foreach ($table_names as $table_name) {		
+foreach ($table_names as $table_name) {
 	$destfile 	= "$table_name.sql";
 
 	$query_inicial = "DROP TABLE IF EXISTS $table_name;
-	 
+
 CREATE TABLE `$table_name` (
 	`area` mediumint(9) NOT NULL DEFAULT '0',
 	`serie` mediumint(9) NOT NULL DEFAULT '0',
@@ -59,15 +59,15 @@ CREATE TABLE `$table_name` (
 		$final = $data[10];
 		$movil = (($data[13] == 'CPP') ? '1' : '0');
 		$valores[] = sprintf("(%s,%s,%s,%s,%s)",$area,$serie,$inicial*1,$final,$movil);
-		
+
 		// Caso especial para los códigos de área de 2 dígitos
 		if (strlen($area) == 2) {
 			$newarea = $area.$serie;
 			$area = substr($newarea,0,3);
 			$serie = substr($newarea,3,3);
 			$valores[] = sprintf("(%s,%s,%s,%s,%s)",$area,$serie,$inicial*1,$final,$movil);
-		}	
-		
+		}
+
 		if (count($valores) >= $batch_size) {
 			fputs($handle2,$query_base . implode(',',$valores) . ";\n");
 			$valores = array();
